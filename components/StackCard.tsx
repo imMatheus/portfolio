aa377d64e8aad983b49ba7279e62e60ac4c86cab
imMatheus <matheusmendesbarata03@gmail.com>
2022-03-05 20:16:56 +0100

    init

---
+++ components/StackCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Star } from 'react-feather'
+
+interface StackCardProps {}
+
+const StackCard: React.FC<StackCardProps> = ({ children }) => {
+	return (
+		<article className="group relative cursor-pointer transition-transform hover:rotate-2 even:hover:-rotate-2">
+			<div className="absolute -bottom-3 -right-3 h-full w-full rounded-md border border-black bg-white transition-all lg:-bottom-4 lg:-right-4">
+				<div className="h-[25px] border-b border-black"></div>
+			</div>
+			<div className="absolute -bottom-1.5 -right-1.5 h-full w-full rounded-md border border-black bg-white transition-all lg:-bottom-2 lg:-right-2">
+				<div className="h-[25px] border-b border-black"></div>
+			</div>
+			<div className="relative h-full w-full rounded-md border border-black bg-white">
+				<div className="flex gap-1 border-b border-black p-2">
+					<div className="h-2 w-2 rounded-full border border-black"></div>
+					<div className="h-2 w-2 rounded-full border border-black"></div>
+					<div className="h-2 w-2 rounded-full border border-black"></div>
+				</div>
+				<div className="p-2">
+					<p className="font-dark mb-2 font-grotesk text-lg">{children}</p>
+					<div className="flex gap-4">
+						<div className="flex items-center gap-1 text-xs">
+							<div className="h-2 w-2 rounded-full bg-sky-700"></div> Typescript
+						</div>
+						<div className="flex items-center gap-0.5 text-xs">
+							<Star className="-mt-[1px] h-4 w-4" /> 14&apos;344
+						</div>
+					</div>
+				</div>
+			</div>
+		</article>
+	)
+}
+
+export default StackCard
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				grotesk: ["'Space Grotesk'", 'sans-serif']
+			},
+			colors: {
+				blurple: '#5865f2',
+				green: '#57f287',
+				yellow: '#fee75c',
+				fuchsia: '#eb459e',
+				red: '#ed4245',
+				black: '#23272a'
+			}
+		}
+	},
+	plugins: []
+}
