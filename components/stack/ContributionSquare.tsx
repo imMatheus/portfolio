a195ce7a4708bb46a418d88fa1b63d0d6823f7d4
imMatheus <matheusmendesbarata03@gmail.com>
2022-03-08 00:02:25 +0100

    changed dialog posistion

--- components/stack/ContributionSquare.tsx
+++ components/stack/ContributionSquare.tsx
@@ -5,9 +5,10 @@ interface ContributionSquareProps {
 	contributionLevel: string
 	date: any
 	color: string
+	index: number
 }
 
-const ContributionSquare: React.FC<ContributionSquareProps> = ({ count, color, contributionLevel, date }) => {
+const ContributionSquare: React.FC<ContributionSquareProps> = ({ count, color, contributionLevel, date, index }) => {
 	// const color =
 	// 	contributionLevel === 'NONE'
 	// 		? 'bg-gray-300'
@@ -18,19 +19,41 @@ const ContributionSquare: React.FC<ContributionSquareProps> = ({ count, color, c
 	// 		: contributionLevel === 'THIRD_QUARTILE'
 	// 		? 'bg-rose-700'
 	// 		: 'bg-rose-900'
+	const minIndex = 50
+	const maxIndex = 330
+
+	const isLeft = index < minIndex
+	const isRight = index > maxIndex
+
+	const yLevel = (index - 1) % 7
+	const isTopRank = yLevel <= 2
 	return (
 		<div
-			className={`group relative h-2 w-2 rounded-sm border border-black/10  md:h-2.5 md:w-2.5`}
+			className={`group relative h-2 w-2 rounded-sm border border-black/10 md:h-2.5 md:w-2.5 xl:h-3 xl:w-3 2xl:h-4 2xl:w-4`}
 			style={{ backgroundColor: color }}
 		>
-			<div className="absolute -top-2 right-1/2 z-10 hidden w-max -translate-y-full translate-x-1/2 rounded-md bg-black/90 py-1 px-4 text-xs text-white hover:block group-hover:block md:text-sm">
+			{/* The dialog over the hovered square */}
+			<div
+				className={`absolute ${isTopRank ? '-bottom-2' : '-top-2'} ${
+					isLeft ? '-left-4' : isRight ? '-right-4' : 'left-1/2'
+				} z-10 hidden w-max ${isTopRank ? 'translate-y-full' : '-translate-y-full'} ${
+					!isLeft && !isRight && '-translate-x-1/2'
+				} rounded-md bg-black/90 py-1 px-4 text-xs text-white hover:block group-hover:block md:text-sm`}
+			>
 				{count == 0
 					? `No contributions on ${date}`
 					: count == 1
 					? '1 contribution on ' + date
 					: count + ' contributions on ' + date}
 			</div>
-			<div className="absolute -top-1 right-1/2 hidden h-2 w-2 translate-x-1/2 -translate-y-full rotate-45 bg-gradient-to-br from-[#0000_50%] to-[rgb(35_39_42_/_0.9)_50%] hover:block group-hover:block"></div>
+			{/* The triangle */}
+			<div
+				className={`absolute ${
+					isTopRank
+						? '-bottom-1 z-10 translate-y-full -rotate-[135deg]'
+						: '-top-1 -translate-y-full rotate-45'
+				} right-1/2 hidden h-2 w-2 translate-x-1/2 bg-gradient-to-br from-[#0000_50%] to-[rgb(35_39_42_/_0.9)_50%] hover:block group-hover:block`}
+			></div>
 		</div>
 	)
 }
--- components/stack/ContributionView.tsx
+++ components/stack/ContributionView.tsx
@@ -7,24 +7,26 @@ interface ContributionViewProps {
 
 const ContributionView: React.FC<ContributionViewProps> = ({ contributionsCollection }) => {
 	console.log(contributionsCollection)
+	let index = 0
 	return (
 		<div className="w-full max-w-max overflow-x-visible">
 			<h3 className="mb-1 text-lg font-normal">
-				Total github contributions in the last 365 days: {contributionsCollection.totalContributions}
+				Total github contributions in the last year: {contributionsCollection.totalContributions}
 			</h3>
 			<div className="relative flex w-full justify-end">
 				<div className="absolute -bottom-2 -right-2 h-full w-full rounded-md border border-black bg-white transition-all lg:-bottom-4 lg:-right-4"></div>
 				<div className="absolute -bottom-1 -right-1 h-full w-full rounded-md border border-black bg-white transition-all lg:-bottom-2 lg:-right-2"></div>
-				<div className="relative grid grid-flow-col grid-rows-[repeat(7,1fr)] gap-0.5 rounded-md border border-black bg-white p-2">
+				<div className="relative grid grid-flow-col grid-rows-[repeat(7,1fr)] gap-0.5 rounded-md border border-black bg-white p-2 xl:gap-1">
 					{/* making sure weekdays line up */}
 					{contributionsCollection.weeks[0].contributionDays[0].weekday !== 0 &&
 						Array.from(Array(1).keys()).map((i) => <div key={i}></div>)}
 
-					{contributionsCollection.weeks.map((week: any) =>
-						week.contributionDays.map((day: any) => (
+					{contributionsCollection.weeks.map((week: any, i: number) =>
+						week.contributionDays.map((day: any, j: number) => (
 							<ContributionSquare
 								key={day.date}
 								date={day.date}
+								index={++index}
 								color={day.color}
 								count={day.contributionCount}
 								contributionLevel={day.contributionLevel}
