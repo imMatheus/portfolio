c00be5eb0d926f1b4e15d382428b56e0a4c7105a
imMatheus <matheusmendesbarata03@gmail.com>
2022-03-22 00:40:40 +0100

    changed stack cards

--- components/stack/StackCard.tsx
+++ components/stack/StackCard.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
 
-interface StackCardProps {
-	title: string
-	date: string
-}
-
-const StackCard: React.FC<StackCardProps> = ({ title, date, children }) => {
+const StackCard: React.FC = ({ children }) => {
 	return (
-		<article className="relative">
-			<div className="relative space-y-1.5 rounded-md bg-black p-4 transition-transform group-odd:group-hover:-rotate-2 group-even:group-hover:rotate-2 md:space-y-3">
-				<h3 className="text-lg font-bold tracking-wide md:text-xl">{title}</h3>
-				<p className="text-sm leading-5 text-gray-300 md:text-base">{children}</p>
-				<p className="text-xs text-gray-500">{date}</p>
-			</div>
-		</article>
+		<div className="flex h-full w-full items-center justify-center overflow-hidden">
+			<h3 className="text-overflow-hidden break-all p-2 text-center text-lg font-semibold md:p-4 md:text-2xl ">
+				{children}
+			</h3>
+		</div>
 	)
 }
 
--- components/stack/StackGrid.tsx
+++ components/stack/StackGrid.tsx
@@ -3,84 +3,42 @@ import StackCard from './StackCard'
 
 const StackGrid: React.FC = ({}) => {
 	return (
-		<div className="grid grid-cols-2 gap-3 md:gap-4 lg:grid-cols-3">
-			<div className="flex flex-col gap-3 md:gap-4">
-				<div className="group rounded-md bg-amber-500 text-amber-500">
-					<StackCard title="Javascript" date="2020-2022">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus assumenda dicta quisquam
-						expedita porro, optio saepe nobis sint quae obcaecati.
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-blue-600 text-blue-600">
-					<StackCard title="Typescript" date="2021-2022">
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga doloremque repellendus excepturi,
-						deserunt quia pariatur?
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-rose-600 text-rose-600">
-					<StackCard title="HTML" date="2019-2022">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nostrum voluptates nesciunt qui
-						saepe facere ipsam similique voluptate, exercitationem doloremque tenetur consequatur?
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-fuchsia text-fuchsia">
-					<StackCard title="Sass/Scss" date="2020-2022">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nostrum voluptates nesciunt qui
-						saepe facere ipsam similique voluptate, exercitationem doloremque tenetur consequatur?
-					</StackCard>
-				</div>
+		<div className="grid aspect-square grid-cols-4 grid-rows-4 gap-1 bg-slate-700 p-1 md:aspect-[10/3] md:grid-rows-3 lg:grid-cols-6 lg:grid-rows-4">
+			<div className="bg-red-500 text-red-100 lg:col-span-2">
+				<StackCard>HTML</StackCard>
 			</div>
-			<div className="flex flex-col gap-3 md:gap-4">
-				<div className="hidden"></div>
-				<div className="group rounded-md bg-sky-400 text-sky-400">
-					<StackCard title="CSS" date="2019-2022">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ullam possimus consequuntur?
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-green-500 text-green-500">
-					<StackCard title="Mongo DB" date="2021-2022">
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, dolorem.
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-orange-500 text-orange-500">
-					<StackCard title="Firebase" date="2021-2022">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto rem voluptatibus minima hic
-						obcaecati a quaerat earum optio deserunt enim!
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-cyan-400 text-cyan-400">
-					<StackCard title="Tailwind" date="2021-2022">
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae porro minima ut accusamus
-						nostrum voluptates accusantium dicta, sed nam. Fugit fugiat dolore reprehenderit illo?
-					</StackCard>
-				</div>
+			<div className="bg-yellow-400 text-yellow-900 lg:col-span-3">
+				<StackCard>Javascript</StackCard>
 			</div>
-			<div className="flex flex-col gap-3 md:gap-4">
-				<div className="group rounded-md bg-sky-500 text-sky-500">
-					<StackCard title="React" date="2021-2022">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsam praesentium nobis
-						aspernatur totam. Pariatur debitis cupiditate, voluptate esse obcaecati veniam? Totam, provident
-						eius!
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-[#000] text-white">
-					<StackCard title="Next.js" date="2021-2022">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas placeat autem alias fugit
-						accusamus!
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-rose-700 text-rose-700">
-					<StackCard title="NPM" date="2021-2022">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nostrum voluptates nesciunt qui
-						saepe facere ipsam similique voluptate, exercitationem doloremque tenetur consequatur?
-					</StackCard>
-				</div>
-				<div className="group rounded-md bg-lime-500 text-lime-500">
-					<StackCard title="Node" date="2021-2022">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat ratione sunt debitis itaque
-						minus corrupti placeat excepturi? Impedit, quasi. Inventore, soluta.
-					</StackCard>
-				</div>
+			<div className="col-span-2 bg-blue-400 text-blue-900 md:col-span-1">
+				<StackCard>Typescript</StackCard>
+			</div>
+			<div className="bg-cyan-400 text-cyan-900 lg:row-span-3 lg:[writing-mode:_vertical-rl]">
+				<StackCard>Tailwind</StackCard>
+			</div>
+			<div className="bg-sky-400 text-sky-900 lg:col-span-2">
+				<StackCard>CSS</StackCard>
+			</div>
+			<div className="bg-pink-600 text-pink-100">
+				<StackCard>Sass/Scss</StackCard>
+			</div>
+			<div className="bg-green-400 text-green-900 lg:col-span-2">
+				<StackCard>Mongo DB</StackCard>
+			</div>
+			<div className="bg-amber-500 text-amber-900 lg:row-span-2">
+				<StackCard>Firebase</StackCard>
+			</div>
+			<div className="col-span-3 bg-black text-white md:col-span-1">
+				<StackCard>Next.js</StackCard>
+			</div>
+			<div className="col-span-2 bg-sky-400 text-sky-900 md:col-span-1 lg:col-span-3">
+				<StackCard>React</StackCard>
+			</div>
+			<div className="bg-rose-700 text-rose-100 lg:col-span-3">
+				<StackCard>NPM</StackCard>
+			</div>
+			<div className="bg-lime-400 text-lime-900">
+				<StackCard>Node</StackCard>
 			</div>
 		</div>
 	)
