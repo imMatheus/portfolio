eb3195082560021413d17a706e5cdf73090c9f02
imMatheus <matheusmendesbarata03@gmail.com>
2025-12-01 21:59:23 -0800

    fixed hydration error

--- components/projects/ProjectDescription.tsx
+++ components/projects/ProjectDescription.tsx
@@ -2,7 +2,7 @@ import { cn } from 'lib/utils'
 import React, { PropsWithChildren } from 'react'
 
 const ProjectDescription: React.FC<PropsWithChildren & { className?: string }> = ({ children, className }) => {
-	return <p className={cn('mb-2 max-w-4xl text-sm font-normal lg:text-base 2xl:text-xl', className)}>{children}</p>
+	return <div className={cn('mb-2 max-w-4xl text-sm font-normal lg:text-base 2xl:text-xl', className)}>{children}</div>
 }
 
 export default ProjectDescription
